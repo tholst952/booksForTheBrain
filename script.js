59cc447cd3cf67386ff3bedd5086521052112906
trena <20079502+tholst952@users.noreply.github.com>
2023-12-05 15:07:57 -0800

    slider is operational. move on to signup section

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+// slider 0%, 100%, 200%
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider-btn--left');
+const btnRight = document.querySelector('.slider-btn--right');
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+// Goto Next Slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+// Goto Previous Slide
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
